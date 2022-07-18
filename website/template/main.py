695c9939c16a1936c4ae3bb91051c61845711692
Tatenda Chiduwa <tatenda.f.chiduwa@ou.edu>
2022-07-18 20:00:06 +0000

    updated

--- website/template/main.py
+++ website/template/main.py
@@ -1,9 +1,14 @@
 from flask import Flask
 
+app = Flask(__name__)
+
 @app.route('/')
 def create_app():
      app = Flask(__name__)
      return '<h1>Hello World!</h1>'
      app.config['SECRET_KEY'] = 'groupisgreat'
+
+if __name__ == '__main__':
+     app.run(debug=True, host='0.0.0.0', port=5001)
    
-     return app
+     
