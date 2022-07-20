460dcd6378eda289d3da5fe1ca5a1a81ea7ed048
Ian Kamweru <ikamweru24@amherst.edu>
2022-07-20 08:20:38 +0000

    remediate branches and update page links

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from models import *
 from forms import *
+import random
 import requests
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'groupisgreat'
@@ -12,15 +13,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 @app.route('/')
+@app.route('/static/index.html')
 def create_app():
-     # app = Flask(__name__)
-     # return '<h1>Hello World!</h1>'
-     # app.config['SECRET_KEY'] = 'groupisgreat'
      return render_template('index.html', title="Trivia Game")
+
+@app.route('/login')
+@app.route('/static/login.html')
+def login():
+     # log in
+     return render_template('login.html')
 
 @app.route('/category', methods = ['GET','POST'])
 def start():
-     return render_template('category.html')  #show categories
+     #show categories
+     return render_template('category.html')  
 
 @app.route('/difficulty', methods = ['GET','POST'])
 def get_difficulty():
@@ -65,9 +71,13 @@ def quiz():
 @app.route('/run',methods = ['GET', 'POST'])
 def run():
      if current_question< len(data):
-        return render_template('question.html', data = data, current_question = current_question) 
+          q = data[current_question]
+          options = q['incorrectAnswers'] + [q['correctAnswer']]
+          random.shuffle(options)
+          # return render_template('question.html', data = data, current_question = current_question) 
+          return render_template('question.html', question = q, options = options)
      else:
-        return '<h1> Correct Answers: ' + str(correct) + '</h1>'
+          return '<h1> Correct Answers: ' + str(correct) + '</h1>'
         
 @app.route("/answered", methods = ['POST'])
 def check_answer():
@@ -82,11 +92,10 @@ def check_answer():
      current_question +=1  
      return render_template('answer.html', answered = answered, correct = correctAnswer) 
 
+@app.route('/register')
+@app.route('/static/register.html')
+def register():
+     return render_template('register.html')
      
 if __name__ == '__main__':
      app.run(debug=True, host='0.0.0.0', port=5001)
-
-
-
-
-
